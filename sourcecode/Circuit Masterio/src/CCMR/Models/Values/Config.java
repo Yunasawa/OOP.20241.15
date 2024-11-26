b20377a7da27b54dc24e34f7bea7e4173e65ffe0
Yunasawa <yunasawa100@gmail.com>
2024-11-26 15:09:24 +0700

    Add selecting function

--- sourcecode/Circuit Masterio/src/CCMR/Models/Values/Config.java
+++ sourcecode/Circuit Masterio/src/CCMR/Models/Values/Config.java
@@ -15,4 +15,5 @@ public class Config
     public static final Color ElementColor = new Color(0.25, 0.25, 0.25, 1);
     public static final Color HoverColor = Utilities.ToColor("#7a7d42");
     public static final Color CollisionColor = Utilities.ToColor("#bd3333");
+    public static final Color SelectedColor = Utilities.ToColor("#33bdb8");
 }
