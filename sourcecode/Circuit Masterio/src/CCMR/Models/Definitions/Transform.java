f251e8e2079e187c00bc345ead7ce99c8249a41b
Yunasawa <yunasawa100@gmail.com>
2024-11-28 12:36:21 +0700

    ...

--- sourcecode/Circuit Masterio/src/CCMR/Models/Definitions/Transform.java
+++ sourcecode/Circuit Masterio/src/CCMR/Models/Definitions/Transform.java
@@ -6,9 +6,9 @@ public class Transform
 {
 	public Vector2 Position;
 	public byte Rotation;
-	public Vector2 Size;
+	//public Vector2 Size;
 	
-	public Transform(Vector2Int position, byte rotation, Vector2Int size) { Position = position.ToVector2(); Rotation = rotation; Size = size.ToVector2(); }
+	public Transform(Vector2Int position, byte rotation, Vector2Int size) { Position = position.ToVector2(); Rotation = rotation; }//Size = size.ToVector2(); }
 	public Transform(Vector2Int position, byte rotation) { this(position, rotation, new Vector2Int()); }
 	public Transform(Vector2Int position) { this(position, (byte)0); }
 	public Transform() { this(new Vector2Int()); }
---
+++ sourcecode/Circuit Masterio/src/CCMR/Views/Environments/Collider.java
@@ -0,0 +1,59 @@
+package CCMR.Views.Environments;
+
+import CCMR.Models.Types.*;
+import CCMR.Models.Values.Config;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.*;
+
+public class Collider extends Rectangle
+{
+	public Vector2 TopLeft = new Vector2();
+	public Vector2 BottomRight = new Vector2();
+	
+	public Vector2 Size;
+	
+	public Collider(double topLeftX, double topLeftY, double bottomRightX, double bottomRightY) 
+	{ 
+		this(new Vector2(topLeftX, topLeftY), new Vector2(bottomRightX, bottomRightY)); 
+	}
+	public Collider(Vector2 topLeft, Vector2 bottomRight)
+	{
+		TopLeft = topLeft;
+		BottomRight = bottomRight;
+		
+		Size = BottomRight.Subtract(TopLeft);
+		
+		this.setX(TopLeft.X * Config.CellSize + 10);
+		this.setY(TopLeft.Y * Config.CellSize + 10);
+		this.setWidth((BottomRight.X - TopLeft.X) * Config.CellSize - 20);
+		this.setHeight((BottomRight.Y - TopLeft.Y) * Config.CellSize - 20);
+		
+		AddStyle();
+		AddIntersetManipulator();
+	}
+	
+	private void AddStyle()
+	{
+		this.setStroke(Color.TRANSPARENT);
+		this.setFill(Color.TRANSPARENT);
+		this.setStrokeWidth(0);
+	}
+	
+	private void AddIntersetManipulator()
+	{
+		this.setOnMouseEntered(event ->
+		{
+			
+		});
+		
+		this.setOnMouseExited(event ->
+		{
+			
+		});
+		
+		this.setOnMouseClicked(event ->
+		{
+			
+		});
+	}
+}
