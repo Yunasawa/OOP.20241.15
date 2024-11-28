1f48fd9f1b384c0afe0c067322d463d01f70b70c
Yunasawa <yunasawa100@gmail.com>
2024-11-29 00:24:21 +0700

    ...

--- sourcecode/Circuit Masterio/src/CCMR/Views/Environments/WireLine.java
+++ sourcecode/Circuit Masterio/src/CCMR/Views/Environments/WireLine.java
@@ -4,18 +4,18 @@ import CCMR.Models.Values.Config;
 import CCMR.Models.Values.Data;
 import CCMR.Models.Values.View;
 import CCMR.Models.Types.Vector2;
-import javafx.scene.paint.Color;
 import javafx.scene.shape.Polyline;
-
 import java.util.ArrayList;
 import java.util.List;
 
-public class WireLine extends Polyline {
+public class WireLine extends Polyline 
+{
     private List<Vector2> gridPoints;
 
-    public WireLine() {
+    public WireLine() 
+    {
         this.gridPoints = new ArrayList<>();
-        this.gridPoints.add(new Vector2(9, 2));   // Example grid points
+        this.gridPoints.add(new Vector2(9, 2));
         this.gridPoints.add(new Vector2(6, 5));
         this.gridPoints.add(new Vector2(8, 10));
 
@@ -27,22 +27,26 @@ public class WireLine extends Polyline {
         View.GridPane.getChildren().add(1, this);
     }
 
-    private void syncPoints() {
+    private void syncPoints() 
+    {
         this.getPoints().clear();
-        for (Vector2 point : gridPoints) {
+        for (Vector2 point : gridPoints) 
+        {
             double x = (point.X * Config.CellSize - Data.GridOffset.X) * Data.ScaleValue;
             double y = (point.Y * Config.CellSize - Data.GridOffset.Y) * Data.ScaleValue;
             this.getPoints().addAll(x, y);
         }
     }
 
-    public void updateScale(double newScaleValue) {
+    public void updateScale(double newScaleValue) 
+    {
         Data.ScaleValue = newScaleValue;
         syncPoints();
         this.setStrokeWidth(Data.StrokeWidth);
     }
 
-    public void updateOffset(Vector2 newGridOffset) {
+    public void updateOffset(Vector2 newGridOffset) 
+    {
         Data.GridOffset = newGridOffset;
         syncPoints();
     }
