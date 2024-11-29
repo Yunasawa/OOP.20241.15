278c1dadd82ef4ef1fa8a5971bb5b455279af38b
Yunasawa <yunasawa100@gmail.com>
2024-11-30 00:10:04 +0700

    ...

--- sourcecode/Circuit Masterio/src/CCMR/Models/Definitions/Transform.java
+++ sourcecode/Circuit Masterio/src/CCMR/Models/Definitions/Transform.java
@@ -8,8 +8,8 @@ public class Transform
 	public byte Rotation;
 	//public Vector2 Size;
 	
-	public Transform(Vector2Int position, byte rotation, Vector2Int size) { Position = position.ToVector2(); Rotation = rotation; }//Size = size.ToVector2(); }
-	public Transform(Vector2Int position, byte rotation) { this(position, rotation, new Vector2Int()); }
-	public Transform(Vector2Int position) { this(position, (byte)0); }
-	public Transform() { this(new Vector2Int()); }
+	public Transform(Vector2 position, byte rotation, Vector2 size) { Position = position; Rotation = rotation; }//Size = size.ToVector2(); }
+	public Transform(Vector2 position, byte rotation) { this(position, rotation, new Vector2()); }
+	public Transform(Vector2 position) { this(position, (byte)0); }
+	public Transform() { this(new Vector2()); }
 }
