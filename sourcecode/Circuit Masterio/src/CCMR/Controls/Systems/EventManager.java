876e47bd907c2e010d344710b7d6ef1ac8fda3b7
Yunasawa <yunasawa100@gmail.com>
2024-12-02 15:06:58 +0700

    Remove unused codes

--- sourcecode/Circuit Masterio/src/CCMR/Controls/Systems/EventManager.java
+++ sourcecode/Circuit Masterio/src/CCMR/Controls/Systems/EventManager.java
@@ -8,21 +8,14 @@ import javafx.scene.input.KeyCode;
 public class EventManager 
 {
 	public Row<IKeyPressListenable> IKeyPressListeners = new Row<>();
-	public Row<IKeyReleaseListenable> IKeyReleaseListeners = new Row<>();
 	
 	public EventManager()
 	{
         Global.GridScene.setOnKeyPressed(event -> { InvokeOnKeyPressedEvent(event.getCode()); });
-        Global.GridScene.setOnKeyReleased(event -> { InvokeOnKeyReleasedEvent(event.getCode()); });
 	}
 	
 	public void InvokeOnKeyPressedEvent(KeyCode key)
 	{
 		for (IKeyPressListenable listener : Global.EventManager.IKeyPressListeners) listener.OnKeyPressed(key);
-	}
-	
-	public void InvokeOnKeyReleasedEvent(KeyCode key)
-	{
-		for (IKeyReleaseListenable listener : Global.EventManager.IKeyReleaseListeners) listener.OnKeyReleased(key);
 	}
 }
