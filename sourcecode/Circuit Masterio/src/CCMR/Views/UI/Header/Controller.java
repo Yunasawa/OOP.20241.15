300c380f9e8d8a885428e64e44dc3d23deedda9d
HuyLong2k4 <maihuylong102@gmail.com>
2024-12-06 22:43:04 +0700

    baseUI

---
+++ sourcecode/Circuit Masterio/src/CCMR/Views/UI/Header/Controller.java
@@ -0,0 +1,42 @@
+package CCMR.Views.UI.Header;
+
+import javafx.fxml.FXML;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
+
+
+import java.awt.*;
+import java.sql.SQLOutput;
+
+public class Controller {
+    @FXML
+    private Canvas canvas;
+
+    @FXML
+    public void initialize() {
+        if(canvas != null) {
+            drawGrid();
+        } else {
+            System.out.println("Loi");
+
+        }
+    }
+
+    private void drawGrid() {
+        GraphicsContext gc = canvas.getGraphicsContext2D();
+        double width = canvas.getWidth();
+        double height = canvas.getHeight();
+        double cellSize = 20;
+
+        gc.setStroke(Color.LIGHTGRAY);
+
+        for (double x = 0; x <= width; x += cellSize) {
+            gc.strokeLine(x, 0, x, height);
+        }
+
+        for (double y = 0; y <= height; y += cellSize) {
+            gc.strokeLine(0, y, width, y);
+        }
+    }
+}
